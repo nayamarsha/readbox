d2f0144954a7bb40c793d46d26d9793fecf596e8
nayamarsha <nayamarsha04@gmail.com>
2025-06-09 21:42:17 +0700

    fix database connection

--- books-service/index.js
+++ books-service/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 .then(() => {
     console.log('Connected to MongoDB');
 })
