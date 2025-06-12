e9dd86310bc04b2ff7cecff52061ab597818d53f
Yuda Putera <142188478+yuddptrr@users.noreply.github.com>
2025-06-12 18:34:26 +0700

    transaction schema fix (type jadi 3)

---
+++ transaction-service/models/transaction.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const TransactionSchema = new mongoose.Schema({
+    transactionId: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['borrow', 'return','renew'], 
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Transaction', TransactionSchema);
