9f9850eefefc79d0fe18f0320377e4e4ffef707d
Yudd <yudaputera11@gmail.com>
2025-06-15 17:12:59 +0700

    refactor routes for improved structure and readability

--- transaction-service/index.js
+++ transaction-service/index.js
@@ -20,8 +20,8 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use(cors());
 app.use(express.json());
 
-const booksRoutes = require('./routes/trRoutes');
-app.use('/books', booksRoutes);
+const transactionRoutes = require('./routes/trRoutes');
+app.use('/transaction', transactionRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
