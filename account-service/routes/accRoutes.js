a9c4b11a5441f662266e12f08ca0b63ba6bcf1a4
marshall <nayyaramarshall@gmail.com>
2025-06-15 17:29:48 +0700

    fix delete

--- account-service/routes/accRoutes.js
+++ account-service/routes/accRoutes.js
@@ -107,7 +107,7 @@ router.put('/:username', verifyToken, async (req, res) => {
 // DELETE akun (admin only)
 router.delete('/:id', authorizeRoles('admin'), async (req, res) => {
     try {
-        const deleted = await Account.findByIdAndDelete(req.params.id);
+        const deleted = await Account.findOneAndDelete({ id: parseInt(req.params.id) });
         if (!deleted) {
             return res.status(404).json({ message: 'Akun tidak ditemukan' });
         }
@@ -116,5 +116,6 @@ router.delete('/:id', authorizeRoles('admin'), async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
 
 module.exports = router;
