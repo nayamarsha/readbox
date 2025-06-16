1e14dfaefe6c8e3c1cfde7fa94f7e8c83044808b
marshall <nayyaramarshall@gmail.com>
2025-06-16 15:17:05 +0700

    RATE LIMITTTT

--- Gateway.js
+++ Gateway.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const rateLimit = require('express-rate-limit'); // Add this line
 const app = express();
 const port = 3000;
+
+// Rate limiter middleware: max 50 requests per 10 minutes per IP
+const limiter = rateLimit({
+ windowMs: 10 * 60 * 1000, // 10 minutes
+ max: 50, // limit each IP to 100 requests per windowMs
+ message: { error: 'Too many requests, please try again later.' }
+});
+
+// Apply rate limiter to all requests
+app.use(limiter);
+
 // API Gateway mengarahkan permintaan ke service1
 app.use('/serviceBooks', createProxyMiddleware({
  target: 'http://localhost:3001',
@@ -13,7 +25,7 @@ pathRewrite: { '^/serviceBooks': '' },
 app.use('/serviceTrans', createProxyMiddleware({
  target: 'http://localhost:3002',
  changeOrigin: true,
- pathRewrite: {'^/servicetrans': '',},
+ pathRewrite: {'^/serviceTrans': '',},
 }));
 
 app.use('/serviceAcc', createProxyMiddleware({
